0ebc53fcf568698a35eb5789fab942ef470c3e6c
callmedrt <dr.teresavasquez@gmail.com>
2024-05-11 12:32:23 -0500

    start up

---
+++ src/app/grow/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function GrowView() {
+  return <div>grow</div>;
+}
---
+++ src/app/play/page.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PlayView() {
+  return <div>play</div>;
+}
